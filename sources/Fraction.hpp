d953e32129452672c7fbd22bf52121f50e46d7cc
yonatanb <jonatan250199@gmail.com>
2023-04-14 20:36:21 +0300

    Few declarations in Fraction.hpp

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -6,6 +6,12 @@ namespace ariel
             int denominator; // represents the denominator in the fraction
         public:
             Fraction(int, int); // constructor
+            Fraction operator+(const Fraction&) const; // addition operator for fractions
+            Fraction operator=(const float&) const; // assignment operator for floats; 
+            friend Fraction operator+(const Fraction&, const float&); // addition operator for fractions and floats
+            friend Fraction operator+(const float&, const Fraction&); // addition operator for fractions and floats
+            //friend Fraction operator+(const float&, const float&); // addition operator for floats
+            //friend operator+(const float&, const Fraction&); // addition operator for fractions and floats
             ~Fraction() = default; // default destructor
     };
 
