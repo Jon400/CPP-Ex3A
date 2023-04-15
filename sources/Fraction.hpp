2f50fa097d7afd7d4fe8bd4c1cfcacd47972602d
yonatanb <jonatan250199@gmail.com>
2023-04-15 22:08:29 +0300

    minor changes

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 namespace ariel
 {
     class Fraction{
@@ -5,15 +7,41 @@ namespace ariel
             int numerator; // represents the numerator in the fraction
             int denominator; // represents the denominator in the fraction
         public:
-            Fraction(int, int); // constructor
-            Fraction operator+(const Fraction&) const; // addition operator for fractions
-            Fraction operator=(const float&) const; // assignment operator for floats; 
-            friend Fraction operator+(const Fraction&, const float&); // addition operator for fractions and floats
-            friend Fraction operator+(const float&, const Fraction&); // addition operator for fractions and floats
-            //friend Fraction operator+(const float&, const float&); // addition operator for floats
-            //friend operator+(const float&, const Fraction&); // addition operator for fractions and floats
+            Fraction(int = 1, int = 1); // constructor with default denominator and numerator of 1
+            Fraction(float = 1.0); // constructor for floats
             ~Fraction() = default; // default destructor
+            Fraction operator=(const float&) const; // assignment operator for floats; 
+            operator float() const; // conversion operator to float
+            Fraction operator+(const Fraction&) const; // addition operator for fractions
+            Fraction operator+(const float&) const; // addition operator for fraction and float
+            Fraction operator-(const Fraction&) const; // subtraction operator for fractions
+            Fraction operator-(const float&) const; // subtraction operator for fraction and float
+            Fraction operator*(const Fraction&) const; // multiplication operator for fractions
+            Fraction operator*(const float&) const; // multiplication operator for fraction and float
+            Fraction operator/(const Fraction&) const; // division operator for fractions
+            Fraction operator/(const float&) const; // division operator for fraction and float
+            bool operator==(const Fraction&) const; // equality operator for fractions
+            bool operator==(const float&) const; // equality operator for float and fraction
+            bool operator>(const Fraction&) const; // greater than operator for fractions
+            bool operator>(const float&) const; // greater than operator for fraction and float
+            bool operator<(const Fraction&) const; // less than operator for fractions
+            bool operator<(const float&) const; // less than operator for fraction and float
+            bool operator>=(const Fraction&) const; // +greater than or equal to operator for fractions
+            bool operator>=(const float&) const; // greater than or equal to operator for fraction and float
+            bool operator<=(const Fraction&) const; // less than or equal to operator for fractions
+            bool operator<=(const float&) const; // less than or equal to operator for fraction and float
+            Fraction& operator++(); // prefix increment operator
+            Fraction operator++(int); // postfix increment operator
+            Fraction& operator--(); // prefix decrement operator
+            Fraction operator--(int); // postfix decrement operator
+            friend std::ostream& operator<<(std::ostream&, const Fraction&); // output operator for fractions
+            friend std::ostream& operator<<(std::ostream&, const float&); // output operator for floats
+            friend std::istream& operator>>(std::istream&, Fraction&); // input operator for fractions
+            friend std::istream& operator>>(std::istream&, float&); // input operator for floats
     };
 
+
     int gcd(int, int); // returns the greatest common divisor of a and b, to be used to find the reduced form of a fraction
+    Fraction reduce(Fraction); // reduces a fraction to its reduced form
+    Fraction convert_float_to_fraction(float); // converts a float to a fraction
 }
